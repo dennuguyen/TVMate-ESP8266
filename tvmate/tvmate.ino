1efe13ed246c06d141f2e9604750700683d24ebf
dennuguyen <dennuguyen@gmail.com>
2024-06-30 16:35:46 +1000

    Remove unused code.

--- tvmate/tvmate.ino
+++ tvmate/tvmate.ino
@@ -20,27 +20,16 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t* payload, size_t length)
         case WStype_CONNECTED: {
             IPAddress ip = webSocket.remoteIP(num);
             Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-
-            // send message to client
             webSocket.sendTXT(num, "Connected");
         } break;
         case WStype_TEXT: {
             Serial.printf("[%u] get Text: %s\n", num, payload);
             uint32_t command = (uint32_t)strtol((const char*)payload, NULL, 16);
             IrSender.sendNEC(0x00, command, 0);
-
-            // send message to client
-            // webSocket.sendTXT(num, "message here");
-
-            // send data to all connected clients
-            // webSocket.broadcastTXT("message here");
         } break;
         case WStype_BIN:
             Serial.printf("[%u] get binary length: %u\n", num, length);
             hexdump(payload, length);
-
-            // send message to client
-            // webSocket.sendBIN(num, payload, length);
             break;
         default:
             break;
